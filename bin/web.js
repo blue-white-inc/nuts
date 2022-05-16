8aa87f8552577bde869a710dfbe690389868d70b
jdinh1 <jdinh0608@yahoo.com>
2022-05-16 16:30:00 -0700

    Updated to have an option to check if the update server is behind a reverse proxy

--- bin/web.js
+++ bin/web.js
@@ -26,7 +26,8 @@ var myNuts = nuts.Nuts({
     timeout: process.env.VERSIONS_TIMEOUT,
     cache: process.env.VERSIONS_CACHE,
     refreshSecret: process.env.GITHUB_SECRET,
-    proxyAssets: !Boolean(process.env.DONT_PROXY_ASSETS)
+    proxyAssets: !Boolean(process.env.DONT_PROXY_ASSETS),
+    behindReverseProxy: Boolean(process.env.BEHIND_REVERSE_PROXY),
 });
 
 // Control access to API
